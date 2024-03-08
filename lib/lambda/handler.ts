b3b80b7b18944089c8ecd765e597c7fe61e51966
Evewwal12 <wxy1012candoit@163.com>
2024-03-08 11:50:26 -0800

    lambda2

--- lib/lambda/handler.ts
+++ lib/lambda/handler.ts
@@ -1,7 +1,7 @@
-export async function handler = async (event: string, context: string){
-    console.log('Stage Name is:' + process.env.stage);
-    return{
-      body: 'Hello from a Lambda Function',
-      statusCode: 200,
+export const handler = async (event: string, context: string) => {
+    console.log('Stage Name is: ' + process.env.stage);
+    return {
+        body: 'Hello from a Lambda Function',
+        statusCode: 200,
     };
-}
+};
