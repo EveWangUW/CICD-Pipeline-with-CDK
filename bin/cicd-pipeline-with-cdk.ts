145fcb5f0d85449c27bca4a9ba80e43527f9118b
Evewwal12 <wxy1012candoit@163.com>
2024-03-07 16:02:46 -0800

    initial commit

---
+++ bin/cicd-pipeline-with-cdk.ts
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import { CicdPipelineWithCdkStack } from '../lib/cicd-pipeline-with-cdk-stack';
+
+const app = new cdk.App();
+new CicdPipelineWithCdkStack(app, 'CicdPipelineWithCdkStack', {
+  env:{
+    account:'033752409409',
+    region:'us-west-2',
+  }
+});
+
+app.synth();
+//bootstrap引导 the process
