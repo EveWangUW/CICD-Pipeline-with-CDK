29b3e73f50ad05340b95d93d8380272127449888
Evewwal12 <wxy1012candoit@163.com>
2024-03-08 10:36:29 -0800

    new changes

--- bin/cicd-pipeline-with-cdk.ts
+++ bin/cicd-pipeline-with-cdk.ts
@@ -5,7 +5,7 @@ import { CicdPipelineWithCdkStack } from '../lib/cicd-pipeline-with-cdk-stack';
 
 const app = new cdk.App();
 //class structure being defined
-new CicdPipelineWithCdkStack(app, 'CicdPipelineWithCdkStack', {
+new CicdPipelineWithCdkStack(app, 'CicdPipelineWithCdkStack2', {
   env:{
     account:'033752409409',
     region:'us-west-2',
--- lib/cicd-pipeline-with-cdk-stack.ts
+++ lib/cicd-pipeline-with-cdk-stack.ts
@@ -8,8 +8,8 @@ export class CicdPipelineWithCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const pipeline = new CodePipeline(this,'Pipelinenew',{
-      pipelineName:'TestPipelinenew',
+    const pipeline = new CodePipeline(this,'Pipeline2',{
+      pipelineName:'TestPipeline2',
       synth: new CodeBuildStep('synth', {
         input: CodePipelineSource.gitHub('EveWangUW/CICD-Pipeline-with-CDK', 'main'),
         commands:['npm install',
