768a5e23f81e31bf8dafe8f83e3aea5b746925fc
Evewwal12 <wxy1012candoit@163.com>
2024-03-07 16:20:35 -0800

    update

--- bin/cicd-pipeline-with-cdk.ts
+++ bin/cicd-pipeline-with-cdk.ts
@@ -4,6 +4,7 @@ import * as cdk from 'aws-cdk-lib';
 import { CicdPipelineWithCdkStack } from '../lib/cicd-pipeline-with-cdk-stack';
 
 const app = new cdk.App();
+//class structure being defined
 new CicdPipelineWithCdkStack(app, 'CicdPipelineWithCdkStack', {
   env:{
     account:'033752409409',
--- lib/cicd-pipeline-with-cdk-stack.ts
+++ lib/cicd-pipeline-with-cdk-stack.ts
@@ -1,16 +1,21 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import {CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { ManualApprovalStep } from 'aws-cdk-lib/pipelines';
+//import { MyPipelineAppStage} from './stage';
 
 export class CicdPipelineWithCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CicdPipelineWithCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new CodePipeline(this,'Pipeline',{
+      pipelineName:'TestPipeline',
+      synth: new ShellStep('synth', {
+        input: CodePipelineSource.gitHub('EveWangUW/CICD-Pipeline-with-CDK', 'main'),
+        commands:['npm ci',
+                  'npm run build',
+                  'npx cdk synth']
+      }),
+    });
   }
 }
