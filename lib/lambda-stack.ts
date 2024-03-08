4b200b55893dbea5b44e8362f6f235fc6dbeb275
Evewwal12 <wxy1012candoit@163.com>
2024-03-08 15:56:39 -0800

    lastlast

--- lib/lambda-stack.ts
+++ lib/lambda-stack.ts
@@ -7,7 +7,7 @@ export class MyLambdaStack extends cdk.Stack {
     super(scope, id, props);
 
     new Function(this, 'LambdaFunction', {
-      runtime: Runtime.NODEJS,
+      runtime: Runtime.NODEJS_18_X,
       handler: 'handler.handler', 
       code: Code.fromAsset(path.join(__dirname, 'lambda')),
       environment: {
