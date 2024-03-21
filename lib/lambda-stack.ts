2e51f7da2e605317b7f9e0317351ca02725ca541
Evewwal12 <wxy1012candoit@163.com>
2024-03-21 15:03:35 -0700

    last

--- lib/lambda-stack.ts
+++ lib/lambda-stack.ts
@@ -3,6 +3,7 @@ import { Construct } from 'constructs';
 import { Function, Runtime, Code } from 'aws-cdk-lib/aws-lambda';
 import * as path from 'path';
 
+
 export class MyLambdaStack extends cdk.Stack {
   constructor(scope: Construct, id: string, stageName: string, props?: cdk.StackProps) {
     super(scope, id, props);
